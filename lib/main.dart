b68ee529dad147e7b5c10f41f16acafceaf9e6ce
mmucaria <mmucaria@technogym.com>
2023-03-04 13:54:22 +0100

    firebase env config and fvm manager

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,19 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:go_firebase/go_firebase_app.dart';
 
-import 'firebase_options.dart';
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
+    options: const FirebaseOptions(
+      apiKey: String.fromEnvironment('FIREBASE_API_KEY'),
+      appId: String.fromEnvironment('FIREBASE_APP_ID'),
+      messagingSenderId: String.fromEnvironment('FIREBASE_MESSAGING_SENDER_ID'),
+      projectId: String.fromEnvironment('FIREBASE_PROJECT_ID'),
+      authDomain: String.fromEnvironment('FIREBASE_AUTH_DOMAIN'),
+      storageBucket: String.fromEnvironment('FIREBASE_STORAGE_BUCKET'),
+      measurementId: String.fromEnvironment('FIREBASE_MEASUREMENT_ID'),
+    ),
   );
 
   runApp(const GoFirebaseApp());
