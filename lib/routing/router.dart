771b0a8de717d4aed2bed2cb1243f7183b97e663
mmucaria <mmucaria@technogym.com>
2023-03-08 18:06:29 +0100

    removed useless comments

--- lib/routing/router.dart
+++ lib/routing/router.dart
@@ -6,9 +6,6 @@ import 'package:go_firebase/services/auth/auth_service.dart';
 import 'package:go_firebase/services/dynamic_link_service.dart';
 import 'package:go_router/go_router.dart';
 
-// The reason for this is to create a kind of singleton pattern to avoid a new
-// instance of the router on each hot reload. Since the material app is wrapped
-// inside a builder function this would cause a new instance of builder.
 class GoFirebaseRouter {
   static GoFirebaseRouter? _instance;
   static GoFirebaseRouter get instance => _instance ??= GoFirebaseRouter._();
@@ -18,7 +15,6 @@ class GoFirebaseRouter {
   late final GoRouter router;
 
   /// Initialize [router] with a pretty configured [GoRouter] instance.
-  /// If [router] was already initialized immediatly returns current instance.
   GoRouter initAppRouter(BuildContext context) {
     final initialRoute = DynamicLinkService.instance.initialRoute;
 
